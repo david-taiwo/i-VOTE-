dba71b6b05c482ce9a7a7946da935ab7e7e65b8b
david-taiwo <davidtaiwo003@gmail.com>
2025-10-28 15:30:17 +0100

    completed the design and responsiveness of the hero section

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -11,17 +11,17 @@ const Home = () => {
           
           {/* TEXT COL */}
           <div className="lg:col-span-7 order-2 lg:order-1 relative z-10 space-y-6 text-center lg:text-left">
-            <h1 className="tracking-tight leading-tight flex justify-center lg:justify-start items-center flex-wrap gap-2">
+            <h1 className="max-w-100vw tracking-tight leading-tight flex justify-center lg:justify-start items-center flex-wrap gap-2">
               {/* Replace text with logo */}
               <span className="inline-block align-middle">
                 <Image
-                  src="/logo2.png"
+                  src="/logo3.png"
                   alt="iVOTE logo"
                   width={200}
                   height={700}
-                  className="h-[40px] md:h-[48px] lg:h-[52px] w-auto object-contain"
+                  className="h-[50px] md:h-[58px] lg:h-[72px] w-auto object-contain"
                 />
-              </span>
+              </span> {' '}
               <span className="text-[40px] md:text-[48px] lg:text-[52px] font-bold text-slate-700">
                 Online Voting
               </span>{' '}
@@ -30,11 +30,11 @@ const Home = () => {
               </span>
             </h1>
 
-            <p className="max-w-xl text-[15px] md:text-base leading-relaxed text-slate-600">
-              Let’s make voting and elections easy and secure for you.{' '}
-              <span className="text-slate-500/70 lg:relative lg:-right-2">
+            <p className="max-w-100vw text-[15px] md:text-base text-center lg:text-left leading-relaxed text-slate-600">
+              Let’s make voting and elections easy and secure for you. This is{' '}
+              {/* <span className="text-slate-500/70 lg:relative lg:-right-2">
                 This is
-              </span>{' '}
+              </span>{' '} */}
               designed to ensure a secured voting session with transparency and trust.
             </p>
 
